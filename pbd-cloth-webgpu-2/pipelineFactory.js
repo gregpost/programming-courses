c48dd5b2ace42300f929f22824c8f76da2234172
Gregory Postolsky <ggp91@yandex.ru>
2025-10-17 12:04:51 +0300

    Create pipelineFactory.js

---
+++ pbd-cloth-webgpu-2/pipelineFactory.js
@@ -0,0 +1,37 @@
+// pipelineFactory.js
+import clothRenderWGSL from "./cloth_render.wgsl";
+
+export class PipelineFactory {
+  static createComputePipeline(device, shaderCode) {
+    return device.createComputePipeline({
+      layout: "auto",
+      compute: { module: device.createShaderModule({ code: shaderCode }), entryPoint: "main" },
+    });
+  }
+
+  static createRenderPipeline(device, format) {
+    const module = device.createShaderModule({ code: clothRenderWGSL });
+    return device.createRenderPipeline({
+      layout: "auto",
+      vertex: {
+        module,
+        entryPoint: "vs_main",
+        buffers: [
+          {
+            arrayStride: 16,
+            attributes: [
+              { shaderLocation: 0, offset: 0, format: "float32x3" },
+              { shaderLocation: 1, offset: 12, format: "float32" },
+            ],
+          },
+        ],
+      },
+      fragment: {
+        module,
+        entryPoint: "fs_main",
+        targets: [{ format }],
+      },
+      primitive: { topology: "triangle-list" },
+    });
+  }
+}
