4b1004445a2712ca297eb4186e0e3f1ccb37cefb
Gregory Postolsky <ggp91@yandex.ru>
2025-10-17 12:15:24 +0300

    Update observable.js

--- pbd-cloth-webgpu-2/js/observable.js
+++ pbd-cloth-webgpu-2/js/observable.js
@@ -1,4 +1,10 @@
-// observable.js
+/**
+ * observable.js
+ * Реализация простого Observer для связи UI и модели.
+ * Позволяет подписываться на события и уведомлять слушателей.
+ * Используется в SimulationModel и SimulationView.
+ * Применяет паттерн Observer.
+ */
 export class Observable {
   constructor() { this.listeners = {}; }
   on(event, cb) { (this.listeners[event] = this.listeners[event] || []).push(cb); }
