b27db3bb9bca9f7b0fecd93c15be505c25b1a4aa
Gregory Postolsky <ggp91@yandex.ru>
2025-10-17 12:16:17 +0300

    Update simulationController.js

--- pbd-cloth-webgpu-2/js/simulationController.js
+++ pbd-cloth-webgpu-2/js/simulationController.js
@@ -1,4 +1,9 @@
-// simulationController.js
+/**
+ * simulationController.js
+ * Controller MVC для симуляции ткани.
+ * Запускает анимационный цикл и обновляет View.
+ * Применяет паттерн MVC.
+ */
 export class SimulationController {
   constructor(model, view) {
     this.model = model;
@@ -6,10 +11,7 @@ export class SimulationController {
     this.frameCount = 0;
   }
   start() {
-    const loop = () => {
-      this.view.renderFrame(this.frameCount++);
-      requestAnimationFrame(loop);
-    };
+    const loop = () => { this.view.renderFrame(this.frameCount++); requestAnimationFrame(loop); };
     loop();
   }
 }
