0eb3665ded9d9dfd2254cc40bbf2cfc89d6b13c6
Gregory Postolsky <ggp91@yandex.ru>
2025-10-17 12:14:57 +0300

    Update pipelineFactory.js

--- pbd-cloth-webgpu-2/js/pipelineFactory.js
+++ pbd-cloth-webgpu-2/js/pipelineFactory.js
@@ -1,5 +1,11 @@
-// pipelineFactory.js
-import clothRenderWGSL from "./cloth_render.wgsl";
+/**
+ * pipelineFactory.js
+ * Factory для создания Compute и Render пайплайнов.
+ * Используется для абстракции создания WebGPU пайплайнов.
+ * Позволяет легко переключать шейдеры и формат канвы.
+ * Применяет паттерн Factory.
+ */
+import clothRenderWGSL from "../shaders/cloth_render.wgsl";
 
 export class PipelineFactory {
   static createComputePipeline(device, shaderCode) {
@@ -17,20 +23,10 @@ export class PipelineFactory {
         module,
         entryPoint: "vs_main",
         buffers: [
-          {
-            arrayStride: 16,
-            attributes: [
-              { shaderLocation: 0, offset: 0, format: "float32x3" },
-              { shaderLocation: 1, offset: 12, format: "float32" },
-            ],
-          },
+          { arrayStride: 16, attributes: [{ shaderLocation: 0, offset: 0, format: "float32x3" }, { shaderLocation: 1, offset: 12, format: "float32" }] },
         ],
       },
-      fragment: {
-        module,
-        entryPoint: "fs_main",
-        targets: [{ format }],
-      },
+      fragment: { module, entryPoint: "fs_main", targets: [{ format }] },
       primitive: { topology: "triangle-list" },
     });
   }
