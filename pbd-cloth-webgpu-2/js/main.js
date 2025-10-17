4e91285b7d28489a88df893ad6ccc7e1d521a0c7
Gregory Postolsky <ggp91@yandex.ru>
2025-10-17 12:23:09 +0300

    Update and rename pbd-cloth-webgpu-2/main.js to pbd-cloth-webgpu-2/js/main.js

---
+++ pbd-cloth-webgpu-2/js/main.js
@@ -0,0 +1,32 @@
+/* main.js
+   Инициализация всей симуляции ткани через WebGPU
+   Использует GPUManager, PipelineFactory, стратегии и MVC
+   Author: Grigoriy Postolskiy
+   Date: 2025
+*/
+
+import { GPUManager } from "./gpuManager.js";
+import { PBDStrategy, MassSpringStrategy } from "./strategies.js";
+import { SimulationModel } from "./simulationModel.js";
+import { SimulationView } from "./simulationView.js";
+import { SimulationController } from "./simulationController.js";
+
+export async function initSimulation(canvas, gravityEl, strategyEl) {
+  if (!navigator.gpu) {
+    alert("WebGPU не поддерживается");
+    throw new Error("WebGPU not supported");
+  }
+
+  const gpu = await GPUManager.getInstance(canvas);
+  const model = new SimulationModel(gpu.device, gpu.format);
+  const view = new SimulationView(gpu.device, gpu.context, gpu.format, model);
+  const controller = new SimulationController(model, view);
+
+  gravityEl.addEventListener("change", () => model.setGravity(gravityEl.checked));
+  strategyEl.addEventListener("change", () => {
+    if (strategyEl.value === "pbd") model.setStrategy(new PBDStrategy());
+    else model.setStrategy(new MassSpringStrategy());
+  });
+
+  controller.start();
+}
