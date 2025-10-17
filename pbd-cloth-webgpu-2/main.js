7845033c994a66fdbf61d5483f458a88ab8487b5
Gregory Postolsky <ggp91@yandex.ru>
2025-10-17 12:16:33 +0300

    Update main.js

--- pbd-cloth-webgpu-2/main.js
+++ pbd-cloth-webgpu-2/main.js
@@ -1,15 +1,18 @@
-// main.js
-import { GPUManager } from "./gpuManager.js";
-import { SimulationModel } from "./simulationModel.js";
-import { SimulationView } from "./simulationView.js";
-import { SimulationController } from "./simulationController.js";
-import { PBDStrategy, MassSpringStrategy } from "./strategies.js";
+/**
+ * main.js
+ * Точка входа приложения.
+ * Инициализирует GPU, модель, View и Controller.
+ * Подключает UI (gravity и strategy select).
+ * Запускает анимационный цикл.
+ */
+import { GPUManager } from "./js/gpuManager.js";
+import { SimulationModel } from "./js/simulationModel.js";
+import { SimulationView } from "./js/simulationView.js";
+import { SimulationController } from "./js/simulationController.js";
+import { PBDStrategy, MassSpringStrategy } from "./js/strategies.js";
 
 export async function initSimulation(canvas, gravityEl, strategyEl) {
-  if (!navigator.gpu) {
-    alert("WebGPU не поддерживается");
-    throw new Error("WebGPU not supported");
-  }
+  if (!navigator.gpu) { alert("WebGPU не поддерживается"); throw new Error("WebGPU not supported"); }
 
   const gpu = await GPUManager.getInstance(canvas);
   const model = new SimulationModel(gpu.device, gpu.format);
