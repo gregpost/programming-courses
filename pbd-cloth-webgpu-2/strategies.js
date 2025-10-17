934a7c26b00513d5d2ddf8f951a625a55123ce00
Gregory Postolsky <ggp91@yandex.ru>
2025-10-17 12:05:14 +0300

    Update strategies.js

--- pbd-cloth-webgpu-2/strategies.js
+++ pbd-cloth-webgpu-2/strategies.js
@@ -1,1 +1,13 @@
+// strategies.js
+import clothUpdatePBD from "./cloth_update_pbd.wgsl";
+import clothUpdateMassSpring from "./cloth_update_massspring.wgsl";
+import { PipelineFactory } from "./pipelineFactory.js";
+
+export class SimulationStrategy {
+  constructor(shader) { this.shader = shader; }
+  createPipeline(device) { return PipelineFactory.createComputePipeline(device, this.shader); }
+}
+
+export class PBDStrategy extends SimulationStrategy { constructor() { super(clothUpdatePBD); } }
+export class MassSpringStrategy extends SimulationStrategy { constructor() { super(clothUpdateMassSpring); } }
 
