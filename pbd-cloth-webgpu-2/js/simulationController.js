f0f0ddc7ad466cb1400c42a4c5bb70afd6e4f820
Gregory Postolsky <ggp91@yandex.ru>
2025-10-25 15:14:37 +0300

    Update simulationController.js

--- pbd-cloth-webgpu-2/js/simulationController.js
+++ pbd-cloth-webgpu-2/js/simulationController.js
@@ -5,6 +5,16 @@
  * Применяет паттерн MVC.
  */
 export class SimulationController {
+  /**
+   * Конструктор создает контроллер для управления симуляцией ткани по паттерну MVC
+   * Инициализирует связь между моделью (данные) и представлением (визуализация)
+   * Устанавливает счетчик кадров для отслеживания прогресса анимации
+   * Проверяет наличие обязательных компонентов модели и представления
+   * Логирует процесс создания контроллера для отладки архитектуры приложения
+   * Контроллер выступает посредником, координирующим обновления между компонентами
+   * @param {SimulationModel} model - Модель симуляции, содержащая данные и логику ткани
+   * @param {SimulationView} view - Представление, отвечающее за визуализацию через WebGPU
+   */
   constructor(model, view) {
     console.log(`[SimulationController.constructor] Creating SimulationController`);
     console.log(`[SimulationController.constructor] Model:`, model ? 'provided' : 'missing');
@@ -17,6 +27,15 @@ export class SimulationController {
     console.log(`[SimulationController.constructor] SimulationController initialized with frameCount: ${this.frameCount}`);
   }
   
+  /**
+   * Запускает основной цикл анимации симуляции ткани через requestAnimationFrame
+   * Создает рекурсивную функцию loop, которая вызывается для каждого кадра анимации
+   * Координирует обновление представления и отслеживает количество отрисованных кадров
+   * Обрабатывает ошибки рендеринга для предотвращения остановки всего цикла анимации
+   * Логирует прогресс каждые 60 кадров для мониторинга производительности симуляции
+   * Обеспечивает плавную анимацию с синхронизацией с частотой обновления дисплея
+   * Использует WebGPU для высокопроизводительного рендеринга трехмерной ткани
+   */
   async start() {
     console.log(`[SimulationController.start] Starting animation loop`);
     console.log(`[SimulationController.start] Initial frameCount: ${this.frameCount}`);
