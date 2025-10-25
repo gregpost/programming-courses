0bcb3cb6bee02e4b1020ef62531faa79eacf0981
Gregory Postolsky <ggp91@yandex.ru>
2025-10-25 15:17:00 +0300

    Update simulationModel.js

--- pbd-cloth-webgpu-2/js/simulationModel.js
+++ pbd-cloth-webgpu-2/js/simulationModel.js
@@ -7,45 +7,8 @@
 import { Observable } from "./observable.js";
 
 export class SimulationModel extends Observable {
-  constructor(device, format, strategy = null) {  // ← Make strategy optional with default
-    super();
-    console.log(`[SimulationModel.constructor] Creating SimulationModel`);
-    
-    this.device = device;
-    this.format = format;
-    this.strategy = strategy;  // ← This was missing!
-    this.gravityEnabled = true;
-    this.entities = [];
-    
-    console.log(`[SimulationModel.constructor] SimulationModel initialized with:`, {
-      device: device ? 'provided' : 'missing',
-      format: format,
-      strategy: this.strategy ? this.strategy.constructor.name : 'null', // ← Log strategy name
-      gravityEnabled: this.gravityEnabled,
-      entitiesCount: this.entities.length
-    });
-  }
-  
-  setStrategy(strategy) { 
-    console.log(`[SimulationModel.setStrategy] Setting new strategy:`, {
-      strategyType: strategy?.constructor?.name || 'unknown',
-      previousStrategy: this.strategy?.constructor?.name || 'null'
-    });
-    
-    this.strategy = strategy; 
-    
-    console.log(`[SimulationModel.setStrategy] Emitting strategyChanged event`);
-    this.emit("strategyChanged", strategy);
-    console.log(`[SimulationModel.setStrategy] Strategy changed and event emitted`);
-  }
-  
-  setGravity(enabled) { 
-    console.log(`[SimulationModel.setGravity] Setting gravity enabled: ${enabled} (was: ${this.gravityEnabled})`);
-    
-    this.gravityEnabled = enabled; 
-    
-    console.log(`[SimulationModel.setGravity] Emitting gravityChanged event`);
-    this.emit("gravityChanged", enabled);
-    console.log(`[SimulationModel.setGravity] Gravity changed and event emitted`);
-  }
-}
+  /**
+   * Конструктор создает модель симуляции ткани, наследуя от Observable для системы событий
+   * Инициализирует все состояние симуляции: устройство GPU, формат, стратегию и флаги
+   * Устанавливает начальные значения для гравитации и пустой массив сущностей ECS
+   *
